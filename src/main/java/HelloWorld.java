0197c44b73f89a0e96cf2399cfaab6657788981a
Trishna <2022wa86144@wilp.bits-pilani.ac.in>
2026-09-25 14:28:45 +0530

    Modified source code

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps Lab!");
+        System.out.println("Hello from DevOps Lab - Continuous Integration!");
     }
 
 }
